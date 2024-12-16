f4515ccb8bcfc38d9da873eebf25c67fa22ca679
Sarfaraj Makvana <sarfarajmakvana@Sarfarajs-Mac-mini.local>
2024-12-16 14:17:20 +0530

    Update

--- index.js
+++ index.js
@@ -16,13 +16,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURLLIVECOMPASS;
-
+const URI = "mongodb+srv://sarfarajampv:RajAtlas313@atlascluster.z6giz.mongodb.net/BTDisplayApp?retryWrites=true&w=majority&appName=AtlasCluster"
 // Connect to MongoDB
-mongoose.connect(URI, {useNewUrlParser:true, useUnifiedTopology: true})
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(error => console.error("MongoDB Connection Error: ", error));
+// mongoose.connect(URI)
+//     .then(() => console.log("Connected to MongoDB"))
+//     .catch(error => console.error("MongoDB Connection Error: ", error));
 
+mongoose.connect(URI, {
+        //useNewUrlParser: true,
+        //useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch((err) => console.error("Error connecting to MongoDB:", err));
 // Routes
 app.use("/product", productRoute);
 
